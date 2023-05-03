d90a74f026e10571f5eac2a5fdf45d28e26fe2f1
= <=>
2023-05-03 08:53:19 -0500

    Ejercicio en Clase - Testeo con Jest CORRECCION

--- Parcial3/Jest/test/test.js
+++ Parcial3/Jest/test/test.js
@@ -1,16 +1,10 @@
 let modulo = require('../src/modulo.js');
-let expect = require('chai').expect;
 
 describe("Pruebas a la función areaTriangulo",()=>
 {
     test("TESTEO CON JEST - Si le mandamos 2 y 3, el resultado debería de regresarme un número 3", ()=>
     {
-        //expect(modulo.areaTriangulo(2,3)).to.be.a("number");
-        expect(modulo.areaTriangulo(2,3)).to.equal(3);
-        /* let resultado = modulo.areaTriangulo(2,3);
-
-        expect(resultado).to.be.a("number");
-        expect(resultado).to.equal(3);*/
+        expect(modulo.areaTriangulo(2,3)).toBe(3);
     });
 });
 
